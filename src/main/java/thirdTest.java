c068d78ad112f04beebe0da0dd9c419ca6b8ffa0
Agnieszka Owczarek <Agnieszka.Owczarek@harman.com>
2020-01-18 20:21:06 +0100

    wait added

--- src/main/java/thirdTest.java
+++ src/main/java/thirdTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class thirdTest {
     private WebDriver driver;
@@ -19,6 +21,9 @@ public class thirdTest {
     public void checkIfUserCanLoginToSubaru() {
         baseUrl = "https://subaru-maps.com/#/signup/names";
         driver.get(baseUrl);
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputName")));
 
         driver.findElement(By.id("inputName")).sendKeys("Anna");
     }
