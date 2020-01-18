d183a5b516c539fc00ff6206459fb3057cd31ac6
Agnieszka Owczarek <Agnieszka.Owczarek@harman.com>
2020-01-18 18:36:14 +0100

    open pracuj.pl

--- src/main/java/firstTest.java
+++ src/main/java/firstTest.java
@@ -3,9 +3,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class firstTest {
     public static void main(String[] args) {
+
         System.out.println("Hello in first class");
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.pracuj.pl/");
+        driver.close();
+
     }
 
 }
