c2f3ba0b82d5f94cd1fc9e07a9fdc6638c06696f
Agnieszka Owczarek <Agnieszka.Owczarek@harman.com>
2020-01-18 18:26:10 +0100

    chromedriver added

--- src/main/java/firstTest.java
+++ src/main/java/firstTest.java
@@ -1,6 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class firstTest {
     public static void main(String[] args) {
         System.out.println("Hello in first class");
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
     }
 
 }
