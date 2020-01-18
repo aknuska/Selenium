60100291a52147b216d41b5176b202f82547a569
Agnieszka Owczarek <Agnieszka.Owczarek@harman.com>
2020-01-18 19:44:26 +0100

    with xpath

--- src/main/java/secondTest.java
+++ src/main/java/secondTest.java
@@ -1,8 +1,11 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class secondTest {
     private WebDriver driver;
@@ -17,6 +20,13 @@ public class secondTest {
     public void checkIfUserCanLoginOnGuru99() {
         String baseUrl = "http://demo.guru99.com/test/login.html";
         driver.get(baseUrl);
+        driver.findElement(By.id("email")).sendKeys("abc@gmail.com");
+        driver.findElement(By.id("passwd")).sendKeys("passwd");
+
+        driver.findElement(By.id("SubmitLogin")).submit();
+
+        assertThat(driver.findElement(By.xpath("/html/body/div[2]/div/div/h3")).getText())
+                .contains("Successfully Logged in...");
     }
 
     @After
