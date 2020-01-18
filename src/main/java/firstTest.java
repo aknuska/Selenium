faa4ec3b108f463f466b02bdddf5ba643da1a21f
Agnieszka Owczarek <Agnieszka.Owczarek@harman.com>
2020-01-18 18:52:04 +0100

    first assertion

--- src/main/java/firstTest.java
+++ src/main/java/firstTest.java
@@ -4,13 +4,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class firstTest {
     public static void main(String[] args) {
 
+        String actualTitle = "";
+
         System.out.println("Hello in first class");
-        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.pracuj.pl/");
+        actualTitle = driver.getTitle();
+
+        if (actualTitle.equals("Praca - Pracuj.pl"))  {
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed! " +  actualTitle);
+        }
         driver.close();
-
     }
-
 }
