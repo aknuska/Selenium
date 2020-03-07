fd1f3b63fa6da8844eba7563a8af2cb30e7ce980
Agnieszka Owczarek <Agnieszka.Owczarek@harman.com>
2020-03-07 12:02:23 +0100

    first class

--- src/main/java/firstTest.java
+++ src/main/java/firstTest.java
@@ -3,9 +3,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class firstTest {
     public static void main(String[] args) {
+
+        String baseURL = "https://www.pracuj.pl/";
+
+
         System.out.println("Hello in first class");
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
+        driver.get(baseURL);
+
+        String actualTitle = driver.getTitle();
+        System.out.println("tytul z wrorcony:" + actualTitle);
+
+        String expectedTitle = "Praca - Pracuj.pl";
+
+        if(expectedTitle.equals(actualTitle)){
+            System.out.println("SUKCES");
+        }
+        else {
+            System.out.println("FAIL");
+        }
+
+
+        driver.close();
+
+
+
     }
 
 }
